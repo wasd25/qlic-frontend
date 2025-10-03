ce987774daa6c6084fec7d38d0bbe051eaff5849
Ayrton <u202311077@upc.edu.pe>
2025-10-02 20:57:40 -0500

    feat(sidebar): add a default layout for sidebar

--- src/public/layouts/default-layout.component.vue
+++ src/public/layouts/default-layout.component.vue
@@ -1,11 +1,26 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <div class="layout">
+    <side-bar-navigation />
+    <main class="content">
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import SideBarNavigation from '../components/side-bar-navigation.component.vue'
+</script>
 
+<style scoped>
+.layout {
+  display: flex;
+}
+
+.content {
+  flex-grow: 1;
+  padding: 1rem;
+  background: #f9fafb;
+  height: 100vh;
+  overflow-y: auto;
+}
 </style>
