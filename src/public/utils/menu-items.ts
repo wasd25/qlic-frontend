e5a2a54b10d5a87a21eae586b65e0870679357da
Ayrton <u202311077@upc.edu.pe>
2025-10-03 11:21:23 -0500

    fix: move files from public to shared

--- src/public/utils/menu-items.ts
+++
@@ -1,8 +0,0 @@
-export const menuItems = [
-    { name: 'dashboard', label: 'Dashboard', to: '/dashboard', icon: 'DashboardIcon' },
-    { name: 'alerts', label: 'Alerts', to: '/alerts', icon: 'BellIcon', badge: 3 },
-    { name: 'reports', label: 'Reports', to: '/reports', icon: 'ReportsIcon' },
-    { name: 'business', label: 'Business', to: '/business', icon: 'BusinessIcon' },
-    { name: 'support', label: 'Support', to: '/support', icon: 'HelpIcon' },
-    { name: 'billing', label: 'Billing', to: '/billing', icon: 'CardIcon' }
-]
---
+++ src/shared/utils/menu-items.ts
@@ -0,0 +1,8 @@
+export const menuItems = [
+    { name: 'dashboard', label: 'Dashboard', to: '/dashboard', icon: 'pi pi-home' },
+    { name: 'alerts', label: 'Alerts', to: '/alerts', icon: 'pi pi-bell', badge: 3 },
+    { name: 'reports', label: 'Reports', to: '/reports', icon: 'pi pi-chart-line' },
+    { name: 'business', label: 'Business', to: '/business', icon: 'pi pi-briefcase' },
+    { name: 'support', label: 'Support', to: '/support', icon: 'pi pi-question-circle' },
+    { name: 'billing', label: 'Billing', to: '/billing', icon: 'pi pi-credit-card' }
+]
