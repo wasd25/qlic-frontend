b84219c4ca69d2e1345a3b42269cfd61d8ef3b3a
Ayrton <u202311077@upc.edu.pe>
2025-10-02 20:48:48 -0500

    feat(sidebar): import files to main.js

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// main.js
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router.js'
+
+import PrimeVue from 'primevue/config'
+import 'primeicons/primeicons.css'
+import './style.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(PrimeVue)
+
+app.mount('#app')
