1fd3ceee84001dde843eaf3fc9fb819c39f3ec53
Everkoe <angeloalejos15@gmail.com>
2025-10-04 12:44:23 -0500

    feat(reports): register report module and routes in main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ import PrimeVue from 'primevue/config'
 import 'primeicons/primeicons.css'
 import './style.css'
 
+
 const app = createApp(App)
 
 app.use(router)
 app.use(PrimeVue)
 
 app.mount('#app')
+
