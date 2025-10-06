cacaf796fcce082ff1a1f0964c91988e625edb9e
Pedro Guia <u202212010@upc.edu.pe>
2025-10-06 12:23:49 -0500

    feat(subscriptions): add page subscription.

---
+++ src/domains/subscriptions/pages/subscription-page.component.vue
@@ -0,0 +1,23 @@
+<script>
+import membershipPlan from "../components/membership-plan.component.vue";
+</script>
+
+<template>
+  <section class="subscription-page">
+    <h1>Subscription</h1>
+    <p>Check, manage and change your subscription profile</p>
+    <membershipPlan/>
+  </section>
+</template>
+
+<style scoped>
+.subscription-page {
+  padding: 2rem;
+  text-align: center;
+}
+
+p {
+  color: #555;
+  margin-bottom: 2rem;
+}
+</style>
