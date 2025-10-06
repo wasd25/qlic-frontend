5bf20c174b946b5c3e146ddf18e528b09c4357a3
lowmath <u202215004@upc.edu.pe>
2025-10-06 14:46:47 -0500

    docs: dont forget to use .env after merging.

--- src/domains/subscriptions/services/subscription.service.js
+++ src/domains/subscriptions/services/subscription.service.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/subscriptions";
+const API_URL = "http://localhost:3000/subscriptions"; //change to .env when hosting.
 
 export const subscriptionService = {
     async getSubscriptions() {
         try {
             const response = await axios.get(API_URL);
+            console.log("Subscriptions fetched:", response.data); // Depuración
             return response.data;
         } catch (error) {
             console.error("Error fetching subscriptions: ", error);
